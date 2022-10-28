558af1e1d1b6c35d6b6744c407c8d8f9c4dc5476
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-10-28 15:41:06 +0530

    add greed logic

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ Session(app)
 class Game(db.Model):
     __tablename__ = 'game'
     game_id = db.Column(db.String(50),primary_key=True)
-    round_num = db.column(db.Integer)
+    round_num = db.Column(db.Integer)
     game_end = db.Column(db.Boolean , unique=False, default=False)
     date_created= db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -53,7 +53,7 @@ class Choice(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     username = db.Column(db.String(200), nullable = False) 
     game_id = db.Column(db.String(50), nullable = False)
-    round_num = db.column(db.Integer)
+    round_num = db.Column(db.Integer)
     number_chosen = db.Column(db.Integer , default = 0)
 
     def __repr__(self) -> str:
@@ -64,25 +64,22 @@ class UserSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     username = db.Column(db.String(200), nullable = False) 
     game_id = db.Column(db.String(50), nullable = False)
-    eliminated = db.Column(db.Boolean , unique=False, default=False)
+    points = db.Column(db.Float , nullable = False , default = 0.0)
 
-def user_invalid_game_entry(username,game_id):
-    cond1 = (Game.query.filter_by(game_id=game_id,game_end=False).first() is None)
-    cond2 = (UserSession.query.filter_by(username=username,game_id=game_id,eliminated=True).first() is not None)
-
-    return cond1 or cond2
+def user_invalid_game_entry(game_id):
+    return Game.query.filter_by(game_id=game_id,game_end=False).first() is None
 
 def user_exists_in_game(username,game_id):
-    return UserSession.query.filter_by(username=username,game_id=game_id,eliminated=False).first() is not None
+    return UserSession.query.filter_by(username=username,game_id=game_id).first() is not None
     
 def user_add_to_game(username , game_id):
-    new_user = UserSession(username = username , game_id = game_id , eliminated = False)
+    new_user = UserSession(username = username , game_id = game_id , points = 0.0)
     db.session.add(new_user)
     db.session.commit()
 
 def user_invalid_game_play(username,game_id,round_num):
     cond1 = (Game.query.filter_by(game_id=game_id,round_num=round_num,game_end=False).first() is None)
-    cond2 = (UserSession.query.filter_by(username=username,game_id=game_id,eliminated=False).first() is None)
+    cond2 = (UserSession.query.filter_by(username=username,game_id=game_id).first() is None)
     cond3 = (Choice.query.filter_by(username=username,game_id=game_id,round_num=round_num).first() is not None)
 
     return cond1 or cond2 or cond3
@@ -132,36 +129,38 @@ def admin_end_round(game_id,round_num):
     except:
         return 
 
-def get_result_and_eliminate(game_id,round_num):
+def get_result(game_id,round_num):
     
     freq = dict()
-    choices = Choice.query.filter_by(game_id=game_id).all()
-
+    choices = Choice.query.filter_by(game_id=game_id,round_num=round_num).all()
     for choice in choices:
         freq[choice.number_chosen] = freq.get(choice.number_chosen,0) + 1
 
-    min_freq = 1e8
-
-    for f in freq.values():
-        min_freq = min(min_freq,f)
-
-    result = []
-    eliminated = []
+    print(choices)
+    print(freq)
 
     for choice in choices:
-        if freq[choice.number_chosen] == min_freq:
-            result.append(choice.username)
-        else:
-            eliminated.append(choice.username)
+        
+        username = choice.username
+        number_chosen = choice.number_chosen
+        f = freq[number_chosen]
 
-    eliminated_users = UserSession.query.filter_by(UserSession.username.in_(eliminated),game_id=game_id).all()
+        user = UserSession.query.filter_by(username=username,game_id=game_id).first()
+        user.points = user.points + ((float(number_chosen))/(float(f)))
+        db.session.commit()
+    
+    userlist = UserSession.query.filter_by(game_id=game_id).all()
+    ranklist = []
 
-    for user in eliminated_users:
-        user.eliminated = True
+    print(userlist)
 
-    db.session.commit()
+    for user in userlist:
+        ranklist.append(user.__dict__)
 
-    return result
+    print(ranklist)
+    ranklist = sorted(ranklist, key=lambda d: d['points'] , reverse=True) 
+
+    return ranklist[:10]
 
 db.create_all()
 
@@ -170,7 +169,7 @@ def user_home():
     if request.method == 'POST':
         username , email , game_id = request.form['username'] , request.form['email'] , request.form['game_id']
 
-        if user_invalid_game_entry(username,game_id):
+        if user_invalid_game_entry(game_id):
             return redirect('/clear_session')
 
         if user_exists_in_game(username,game_id):
@@ -214,21 +213,25 @@ def user_game_play():
             number_chosen = request.form['number_chosen']
 
             user_add_choice(username,game_id,round_num,number_chosen)
-            return redirect(f'/user/round_end/{game_id}/{round_num}')
+            return redirect(f'/user/round_end')
 
-    return render_template('user_game_play.html')
+    return render_template('user_game_play.html',game_id=game_id,round_num=round_num)
 
-@app.route('/user/round_end/<string:game_id>/<int:round_num>',methods=['GET','POST'])
-def user_round_end(game_id,round_num):
-
-    current_game = session.get('current_game',{'game_id':-1,'round_num':-1,'game_end':True})
-
-    if current_game['game_id']!=game_id or current_game['round_num']!=round_num:
-        return redirect('/clear_session')
+@app.route('/user/round_end',methods=['GET','POST'])
+def user_round_end():
 
     current_user = session.get('current_user',{'username':'','email':''})
+    current_game = session.get('current_game',{'game_id':-1,'round_num':-1,'game_end':True})
+
     username = current_user['username']
-        
+    game_id = current_game['game_id']
+    round_num = current_game['round_num']
+
+    if game_id==-1 or round_num==-1:
+        return redirect('/clear_session')
+
+    is_last_round = (round_num==NUM_ROUNDS)
+
     if request.method == 'POST':
         if round_num == NUM_ROUNDS:
             return redirect('/clear_session')
@@ -236,7 +239,7 @@ def user_round_end(game_id,round_num):
             session['current_game'] = {'game_id':game_id,'round_num':round_num+1,'game_end':False}
             return redirect('/user/game_play')
 
-    return render_template('user_game_end.html')
+    return render_template('user_round_end.html',game_id=game_id,round_num=round_num,is_last_round=is_last_round)
 
 @app.route("/admin", methods=['GET','POST'])
 def admin_home():
@@ -315,19 +318,20 @@ def andmin_round_end():
         try:
             current_game = session.get('current_game',{'game_id':-1,'round_num':-1,'game_end':True})
             game_id = request.form['game_id']
-            round_num = request.form['round_num']
+            round_num = int(request.form['round_num'])
 
             if current_game['game_id']==game_id and current_game['round_num']==round_num:
+                print("redirecting to result")
                 return redirect(f'/admin/result/{game_id}/{round_num}')
             
         except Exception as E:
             print(E)
-            pass
 
     return redirect('/clear_session')
 
 @app.route('/admin/result/<string:game_id>/<int:round_num>' , methods=['GET','POST'])
 def admin_result(game_id,round_num):
+
 
     if admin_invalid_round_end(game_id,round_num):
         return redirect('/clear_session')
@@ -341,8 +345,9 @@ def admin_result(game_id,round_num):
     else:
         session['current_game'] = {'game_id':game_id,'round_num':round_num+1,'game_end':False}
 
-    result = get_result_and_eliminate(game_id,round_num)
-    return render_template('result.html',game_id=game_id,round_num=round_num,result=result,is_last_round=is_last_round)
+    result = get_result(game_id,round_num)
+    print("got result")
+    return render_template('admin_result.html',game_id=game_id,round_num=round_num,result=result,is_last_round=is_last_round)
 
 
 @app.route("/clear_session",methods=['GET','POST'])
